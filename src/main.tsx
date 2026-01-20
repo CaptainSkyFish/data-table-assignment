c54f67043dbf2987bc7157e756f2784661e6d041
captainskyfish <ajaystd9756@gmail.com>
2026-01-21 02:44:54 +0530

    fix: theme toggle flicker

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import 'primeicons/primeicons.css'
 import { ThemeProvider } from './context/ThemeContext.tsx'
+import "primereact/resources/themes/md-light-indigo/theme.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
